6ba833c3058e32aa496322b4a52887145dc3859c
Freek van Polen <freek@sense-os.nl>
2012-10-30 15:13:32 +0100

    Mocha test now is documentaiton

---
+++ test/test_senseapi.js
@@ -0,0 +1,69 @@
+
+//var chai = require("assert")
+
+var should = chai.should();
+
+describe('SenseApi.', function(){
+	
+	// setting server
+	describe('setServer', function(){
+		it('should return true if set to "live"', function(){
+			SenseApi.setServer('live').should.equal(true);
+		});
+		it('should result in server set to "live"', function(){
+			SenseApi.getServer().should.equal('live');
+		});
+		it('should return true if set to "dev"', function(){
+			SenseApi.setServer('dev').should.equal(true);
+		});
+		it('should result in server set to "dev"', function(){
+			SenseApi.getServer().should.equal('dev');
+		});
+	});
+	
+	// authentication
+    describe('AuthenticateSessionId', function(){
+        it('should return true if credentials are correct', function(){
+            SenseApi.AuthenticateSessionId("GreenhouseTest", "477e9c66c4bc554dc03c1497fb7b7165").should.equal(true);
+        });
+        it('should result in a session id being set', function() {
+            SenseApi.getSessionId().should.not.equal('');
+        });
+        it('should return false if credentials are wrong', function(){
+            SenseApi.AuthenticateSessionId("blaat", "herp").should.equal(false);
+        });
+    });
+
+    // creating sensor
+    describe('SensorsPost', function(){
+    	it('should return true, indicating the sensor was created', function(){
+    		SenseApi.SensorsPost({"sensor":{"name":"thermometer", "display_name":"Thermometer", "device_type":"TX-1000", "data_type":"float"}}).should.equal(true);
+    	});
+    	it('should allow us to retrieve 201 as status', function(){
+    		SenseApi.getResponseStatus().should.equal(201);
+    	});
+    	it('should allow us to get the sensor_id from the "location" response header', function(){
+    		SenseApi.getResponseHeader().should.have.property('Location');
+    	});
+    });
+    
+    // retrieving sensor
+    describe('SensorsGet', function(){
+    	it('should return true, indicating we received a list of sensors', function(){
+    		SenseApi.SensorsGet({}, null).should.be.true;
+    	});
+    });
+    
+    // logout
+    describe('LougoutSessionId', function(){
+    	it('should return true since we are logged in', function(){
+    		SenseApi.LogoutSessionId().should.equal(true);
+    	});
+    	it('should result in session_id being an empty string', function(){
+    		SenseApi.getSessionId().should.equal('');
+    	});
+    });
+
+
+});
+
